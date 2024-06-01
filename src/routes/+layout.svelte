1ff39c46abeddbfd57fea761355d384c70da60f6
artesgo <artesgo@gmail.com>
2024-05-31 22:02:34 -0400

    Pt1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import './app.scss';
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    max-width: 900px;
+    border: 1px solid;
+    box-sizing: border-box;
+    margin: 0 auto;
+  }
+</style>
